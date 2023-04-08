974df6593d95c3f4e66495e560573d2f048564fe
Daniel de Oliveira <daniel@daniel.oliveira>
2023-04-08 17:16:56 -0300

    Alterações feitas no script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 let marca_jogador_atual = "X"; // Variável para acompanhar a marca do jogador atual
 let celulas_jogo = document.getElementsByClassName("celula"); // HTMLCollection que contém todas as células do jogo, obtidas pelo uso da função document.getElementsByClassName()
 let jogo_acabado = false; //  Variável usada para rastrear o estado do jogo, indicando se o jogo acabou ou não 
-
+let botao_reiniciar = document.getElementById("botao-reiniciar") //Variável usada para implementar um evento de click
 
 
 // Função executada quando uma célula é clicada pelo jogador humano
@@ -28,18 +28,21 @@ function jogada_humana(indice) {
 // Função que representa a jogada da IA (Inteligência Artificial)
 
 function jogada_ia() {
- let celulas_vazias = Array.from(celulas_jogo).filter(function(celulas_jogo){return celulas_jogo.innerHTML === '';}); 
- let indice_aleatorio = Math.floor(Math.random() * celulas_vazias.length);
- setTimeout(() => {
-     celulas_vazias[indice_aleatorio].innerHTML = marca_jogador_atual;
-     if (marca_jogador_atual === "X") {
-      marca_jogador_atual = "O";
-     } else {
-      marca_jogador_atual = "X";
-     }
-     verificar_vencedor();
-  }, 500);
- 
+  for (let i = 0; i < celulas_jogo.length; i++) {
+    let celula = celulas_jogo[i];
+    if (celula.innerHTML === "") {
+      setTimeout(() => {
+        celula.innerHTML = marca_jogador_atual; 
+      if (marca_jogador_atual === "X") {
+              marca_jogador_atual = "O";
+             } else {
+              marca_jogador_atual = "X";
+             }
+             verificar_vencedor();
+            }, 500);
+      break;
+    }
+  }
 }
 
 
@@ -85,12 +88,16 @@ function verificar_vencedor() {
 
 
 
-// Seleciona todos os elementos HTML com a classe "celula", adiciona um evento de clique a cada um deles associando a funçao jogada_humana()
+// Adiciona um evento de clique a cada célula do tabuleiro
+// Associa a função jogada_humana() para ser executada quando uma célula é clicada, passando o índice da célula como argumento. 
 
-document.querySelectorAll(".celula").forEach((celula, indice) => {
-  celula.addEventListener("click", () => jogada_humana(indice));
+for (let indice = 0; indice < celulas_jogo.length; indice++) {
+  let celula = celulas_jogo[indice];
+  celula.addEventListener("click", function() {
+    jogada_humana(indice);
   });
+}
   
 // Adiciona um evento de clique ao botão de reiniciar do jogo associando a função reiniciar_jogo()
 
-document.getElementById("botao-reiniciar").addEventListener("click", reiniciar_jogo);
+botao_reiniciar.addEventListener("click", reiniciar_jogo);
